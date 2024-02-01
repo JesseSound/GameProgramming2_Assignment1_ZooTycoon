16985e5a1f0cddc7478fba3c30de2173197467ea
JesseSound <61964089+JesseSound@users.noreply.github.com>
2024-02-01 01:42:49 -0500

    ugh

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 5.0f;
+    private Rigidbody2D rb;
+
+    //public float speed = 0.02f;
+
+    //public float speedY = 0.01f;
+
+    //public float directionX = 1.0f;
+
+    //public float directionY = 1.0f;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0.0f;
+    }
+    void Update()
+
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        // Check for input
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveY = 1f;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveY = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveX = -1f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveX = 1f;
+        }
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.velocity = movement.normalized * moveSpeed;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
